ddf52b67d3dd0cb7b92e5232dc70b111a4536db8
Atrix256 <alan.wolfe@gmail.com>
2015-09-30 21:17:29 -0700

    Started Multiplier
    Much copy/paste from adder

--- Multiplier/Source.cpp
+++ Multiplier/Source.cpp
@@ -7,7 +7,7 @@ typedef boost::multiprecision::cpp_int TINT;
 int main(int argc, char **argv)
 {
     // verify parameters
-    std::cout << "--Adder--\n\nUses an input key file to add bits superpositionally and show results.\nRequires an even number of bits.\n\n";
+    std::cout << "--Multiplier--\n\nUses an input key file to multiply bits superpositionally and show results.\nRequires an even number of bits.\n\n";
     if (argc < 2)
     {
         std::cout << "Usage: <inputFile>\n";;
@@ -24,38 +24,43 @@ int main(int argc, char **argv)
         if (superPositionedBits.size() % 2 == 0)
         {
             std::cout << "file loaded!\n\n";
-            const size_t numBitsAdding = superPositionedBits.size() / 2;
-            std::cout << "Adding " << numBitsAdding << " bits and " << numBitsAdding << " bits to get a " << numBitsAdding + 1 << " bit result\n";
-            std::cout << "Note that you could truncate the high bit if you wanted to\n";
+            const size_t numInputBits = superPositionedBits.size() / 2;
+            std::cout << "Multiplying " << numInputBits << " bits and " << numInputBits << " bits to get a " << (numInputBits * 2) << " bit result\n";
+            std::cout << "Note that you could truncate the high bits if you wanted to\n";
 
             // Calculate the LCM of our keys so we can make results mod this LCM and keep numbers smaller
             TINT keysLCM = 1;
             for (const TINT& v : keys)
                 keysLCM *= v;
 
-            // do our multi bit addition! N bits + N bits = up to N+1 bit result.
+            // do our multi bit multiplication! N bits * N bits = up to N*2 bit result.
             // Note that we can initialize our carry bit to the value 0.  we don't
             // need to initialize it to a superpositioned bit value!
             TINT carryBit = 0;
-            std::vector<TINT> adderResults;
-            adderResults.resize(numBitsAdding+1);
-            for (size_t i = 0; i < numBitsAdding; ++i)
+            std::vector<TINT> results;
+            results.resize(numInputBits * 2);
+
+            // TODO: make the multiplication work here!
+            std::fill(results.begin(), results.end(), 0);
+            /*
+            for (size_t i = 0; i < numBitsMultiplying; ++i)
             {
                 adderResults[i] = FullAdder(superPositionedBits[i], superPositionedBits[i + numBitsAdding], carryBit) % keysLCM;
                 carryBit = carryBit % keysLCM;
             }
             adderResults[numBitsAdding] = carryBit;
+            */
 
             // show superpositional result and error (max and % of each key)
-            std::cout << "\nSuperpositional Adder Result:\n\n";
-            for (size_t i = 0, c = adderResults.size(); i < c; ++i)
+            std::cout << "\nSuperpositional Multiplier Result:\n\n";
+            for (size_t i = 0, c = results.size(); i < c; ++i)
             {
-                std::cout << "--Bit " << i << "--\n\n" << adderResults[i] << "\n\n";
+                std::cout << "--Bit " << i << "--\n\n" << results[i] << "\n\n";
 
                 float maxError = 0.0f;
                 for (int keyIndex = 0, keyCount = keys.size(); keyIndex < keyCount; ++keyIndex)
                 {
-                    float error = 100.0f * float(adderResults[i] % keys[keyIndex]) / float(keys[keyIndex]);
+                    float error = 100.0f * float(results[i] % keys[keyIndex]) / float(keys[keyIndex]);
                     if (error > maxError)
                         maxError = error;
                 }
@@ -64,24 +69,24 @@ int main(int argc, char **argv)
 
             // Permute through results, make sure truth tables add up
             printf("Result Verification:\n");
-            const size_t maxValue = (1 << numBitsAdding) - 1;
+            const size_t maxValue = (1 << numInputBits) - 1;
             for (size_t a = 0; a <= maxValue; ++a)
             {
                 for (size_t b = 0; b <= maxValue; ++b)
                 {
                     // get the index of our key for this specific set of inputs
-                    size_t keyIndex = (a << numBitsAdding) | b;
+                    size_t keyIndex = (a << numInputBits) | b;
 
                     // decode result for this specific key index
                     size_t result = 0;
-                    for (size_t i = 0, c = adderResults.size(); i < c; ++i)
-                        result = result | (size_t((adderResults[i] % keys[keyIndex]) % 2) << i);
+                    for (size_t i = 0, c = results.size(); i < c; ++i)
+                        result = result | (size_t((results[i] % keys[keyIndex]) % 2) << i);
 
                     // show the result
-                    std::cout << "  [" << keyIndex << "]  " << a << " + " << b << " = " << result << "\n";
+                    std::cout << "  [" << keyIndex << "]  " << a << " * " << b << " = " << result << "\n";
 
                     // verify the result
-                    if (result != a + b)
+                    if (result != a * b)
                     {
                         std::cout << "ERROR! incorrect value detected!";
                         // TODO: assert or something?
@@ -103,3 +108,14 @@ int main(int argc, char **argv)
 
     ExitCode_(0);
 }
+
+/*
+TODO:
+* make this work
+* https://en.wikipedia.org/wiki/Binary_multiplier
+
+! NEXT:
+ * divide / modulus: http://courses.cs.vt.edu/~cs1104/BuildingBlocks/divide.030.html
+ * something more time consuming or impressive.  Like shor's algorithm or who knows what else.  CORDIC math perhaps to calculate sine since it's branchless?
+ * can we do bootstrapping with superpositional keys? do we have to do one layer deeper of HE or something?
+*/
